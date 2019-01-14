7d2943d4035bb81c3ed99df0d52223f3ec76a534
nicolasklz <nicolaskleinhentz@gmail.com>
2019-01-14 11:49:12 +0100

    Ajout du dossier src et ajout du commentaire //

--- compilateurYal/yal/exceptions/AnalyseLexicaleException.java
+++
@@ -1,9 +0,0 @@
-package compilateurYal.yal.exceptions;
-
-public class AnalyseLexicaleException extends AnalyseException {
- 
-    public AnalyseLexicaleException(int ligne, int colonne, String m) {
-        super("ERREUR LEXICALE :\n\tligne " + ligne + " colonne " + colonne + "\n\t" + m + " : caractère non reconnu") ;
-    }
-
-}
--- compilateurYal/yal/exceptions/AnalyseSemantiqueException.java
+++
@@ -1,9 +0,0 @@
-package compilateurYal.yal.exceptions;
-
-public class AnalyseSemantiqueException extends AnalyseException {
- 
-    public AnalyseSemantiqueException(int ligne, String m) {
-        super("ERREUR SEMANTIQUE : ligne " + ligne + "\n\t" + m + "\n") ;
-    }
-
-}
