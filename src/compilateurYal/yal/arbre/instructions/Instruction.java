41b24b808ad6c2a9507fda9dcbca071ca7fa32f0
nicolasklz <nicolaskleinhentz@gmail.com>
2019-01-14 11:49:59 +0100

    Ajout du dossier src et ajout du commentaire //

---
+++ src/compilateurYal/yal/arbre/instructions/Instruction.java
@@ -0,0 +1,11 @@
+package compilateurYal.yal.arbre.instructions;
+
+import compilateurYal.yal.arbre.ArbreAbstrait;
+
+public abstract class Instruction extends ArbreAbstrait {
+
+    protected Instruction(int n) {
+        super(n);
+    }
+
+}
